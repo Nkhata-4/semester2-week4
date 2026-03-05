32685ede13b593626d9dda6ae28305922b6a215b
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-03-05 17:28:51 +0000

    worked on portfolio task

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -10,25 +10,20 @@ Student ID: 201930814
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
-    char text[100];
-    char enter_text[100];
-    printf("Enter your text: ");
-    fgets(enter_text, sizeof(enter_text), stdin);
+    char buffer[100];
     
     
     // process the command-line data using appropriate string functions
     
-    int i=0;
-    for ( i=0; i<argc; i++) {
-        if (enter_text[i] != " ") {
-            text[i] = enter_text[i];
-        } else {
-            text[i] = "-";
+    int i=1;
+    for ( i=1; i<argc; i++) {
+        strcat(buffer, argv[i]);
+        if (i != argc - 1) {
+            strcat(buffer, "-");
         }
     }
-        
     
-    printf("%s\n",text); // use only this single print statement in your submitted code
+    printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
 }
